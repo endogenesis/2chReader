d2e5ab68231a3086f73b07cd11970619a5c5c1ca
NIkolay Tsygankov <endogenesis1@gmail.com>
2016-03-25 18:03:40 +0300

    Processing <strong> and <em> tags

--- 2chReader/Utils/AttributedStringBuilder.swift
+++ 2chReader/Utils/AttributedStringBuilder.swift
@@ -9,27 +9,41 @@
 import Foundation
 import UIKit
 
-let defaultFontSize: CGFloat = 20
+let defaultFontSize: CGFloat = 17
 
 let boldFont = UIFont.boldSystemFontOfSize(defaultFontSize)
 let boldAttributes = [NSFontAttributeName : boldFont]
 let boldTag = (regExp:"<strong>.*?</strong>", attributes: boldAttributes)
 
+let italicFont = UIFont.italicSystemFontOfSize(defaultFontSize)
+let italicAttributes = [NSFontAttributeName : italicFont]
+let italicTag = (regExp:"<em>.*?</em>", attributes: italicAttributes)
+
+struct TagToProcess {
+    let regExp: String
+    let attributes: [String : UIFont]
+    let openLength: Int
+    let closedLength: Int
+}
+
+let boldTag1 = TagToProcess(regExp: "<strong>.*?</strong>", attributes: boldAttributes, openLength: 8, closedLength: 9)
+let italicTag1 = TagToProcess(regExp: "<em>.*?</em>", attributes: italicAttributes, openLength: 4, closedLength: 5)
+
+
 class AttibutedStringBuilder {
     
-    let tagsToProcess = [boldTag]
+    let tagsToProcess = [boldTag1, italicTag1]
     
     func attributedString(stringWithTags: String) -> NSAttributedString? {
         
         let attrStr = NSMutableAttributedString(string: stringWithTags)
         
-        var strToProcess = stringWithTags
         for tag in self.tagsToProcess {
-            var rangeOfSearchText = self.findTextWithTag(tag.regExp, str: strToProcess)
+            var rangeOfSearchText = self.findTextWithTag(tag.regExp, str: attrStr.mutableString as String)
             while rangeOfSearchText.length != 0 {
-                let rangeWithoutTags = self.removeTagsFromString(&strToProcess, range: &rangeOfSearchText)
+                let rangeWithoutTags = self.removeTagsFromString(attrStr.mutableString, range: &rangeOfSearchText, open: tag.openLength, closed: tag.closedLength)
                 attrStr.addAttributes(tag.attributes, range: rangeWithoutTags)
-                rangeOfSearchText = self.findTextWithTag(tag.regExp, str: strToProcess)
+                rangeOfSearchText = self.findTextWithTag(tag.regExp, str: attrStr.mutableString as String)
             }
         }
         return attrStr
@@ -39,7 +53,7 @@ class AttibutedStringBuilder {
         
         let regex = try! NSRegularExpression(pattern: regExp, options: .CaseInsensitive)
         
-        let result =  regex.firstMatchInString(str, options: .Anchored, range: NSRange(location: 0, length: str.characters.count))
+        let result =  regex.firstMatchInString(str, options: .ReportProgress, range: NSRange(location: 0, length: str.characters.count))
         
         if let result = result {
             return result.range
@@ -48,14 +62,15 @@ class AttibutedStringBuilder {
         }
     }
     
-    func removeTagsFromString(inout str: String, inout range: NSRange) -> NSRange {
-        let openTagRange = str.startIndex.advancedBy(range.location)...str.startIndex.advancedBy(range.location + 9)
-        str.removeRange(openTagRange)
-        range.length -= 9
+    func removeTagsFromString(str: NSMutableString, inout range: NSRange, open: Int, closed: Int) -> NSRange {
         
-        let closedTagRange = str.startIndex.advancedBy(range.location + range.length - 9)...str.startIndex.advancedBy(range.location + range.length)
-        str.removeRange(closedTagRange)
-        range.length -= 9
+        let openTagRange = NSRange(location: range.location, length: open)
+        str.deleteCharactersInRange(openTagRange)
+        range.length -= open
+        
+        let closedTagRange = NSRange(location: range.location + range.length - closed, length: closed)
+        str.deleteCharactersInRange(closedTagRange)
+        range.length -= closed
         return range
     }
 }
